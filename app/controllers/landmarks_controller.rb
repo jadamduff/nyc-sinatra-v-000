2ae46a0a3218b9e358ad4d7d1ddb38c88f9202b9
Adam Duff <j.adam.duff@gmail.com>
2018-09-11 07:39:22 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -24,7 +24,7 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/edit'
   end
 
-  patch '/landmarks/:id/edit' do
+  patch '/landmarks/:id' do
     landmark = Landmark.find(params[:id])
     landmark.update(params[:landmark])
     redirect '/landmark/' + landmark.id
